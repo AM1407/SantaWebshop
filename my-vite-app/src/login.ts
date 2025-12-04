57e524aead507e7afd20a77a4e125c5013cc609b
AMicciche <alessiomicciche@outlook.com>
2025-12-04 16:17:37 +0100

    feat: initialize login input elements in login.ts

---
+++ my-vite-app/src/login.ts
@@ -0,0 +1,4 @@
+const userName = document.getElementById('usernameInput');
+const password = document.getElementById('passwordInput');
+const checkBox = document.getElementById('remember');
+const loginBtn = document.getElementById('loginBtn');
